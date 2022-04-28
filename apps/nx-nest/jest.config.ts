069b3b96bb4e1daaa0e72f7bee4f23a969be5bf9
masamichhhhi <masamichhhi@gmail.com>
2022-04-28 22:13:09 +0900

    initial commit

---
+++ apps/nx-nest/jest.config.ts
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'nx-nest',
+  preset: '../../jest.preset.ts',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/nx-nest',
+};
