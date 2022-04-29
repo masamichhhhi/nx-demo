fa2397d2bc9a195d1b1d16bd5114a17cac6d3ff4
masamichhhhi <masamichhhi@gmail.com>
2022-04-29 11:10:32 +0900

    First commit

--- apps/server/jest.config.ts
+++ apps/server/jest.config.ts
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'nx-nest',
+  displayName: 'server',
   preset: '../../jest.preset.ts',
   globals: {
     'ts-jest': {
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/nx-nest',
+  coverageDirectory: '../../coverage/apps/server',
 };
--- apps/server/src/app/app.controller.spec.ts
+++ apps/server/src/app/app.controller.spec.ts
@@ -14,10 +14,10 @@ describe('AppController', () => {
   });
 
   describe('getData', () => {
-    it('should return "Welcome to nx-nest!"', () => {
+    it('should return "Welcome to server!"', () => {
       const appController = app.get<AppController>(AppController);
       expect(appController.getData()).toEqual({
-        message: 'Welcome to nx-nest!',
+        message: 'Welcome to server!',
       });
     });
   });
